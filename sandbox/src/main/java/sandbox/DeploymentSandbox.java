1387b859800f5754568abc7060fe56c89232efa6
Jerome Brongniart <jerome.brongniart@exense.ch>
2019-09-26 11:46:58 +0200

    Add sandbox project

---
+++ sandbox/src/main/java/sandbox/DeploymentSandbox.java
@@ -0,0 +1,27 @@
+package sandbox;
+
+/**
+* This is some Javadoc
+* 
+* @author Dorian Cransac
+* 
+*/
+public class DeploymentSandbox{
+	/**
+	 * A private member
+	 * 
+	 */
+	private String aMember;
+	
+	/**
+	 * <p>A method description
+	 * </p>
+	 * @param someParameter a parameter description
+	 * @return a return description
+	 * @see <a href="http://exense.ch">a link</a>
+	 * @since 1.0
+	 */
+	public int doNothing(int someParameter) {
+	    return 0;
+	}
+}
