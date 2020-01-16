695e7939dd4a4acc7ce1cbc48c468c21bb8abeea
Jerome Brongniart <support@exense.ch>
2020-01-16 16:26:53 +0100

    Adding projects

---
+++ sandbox_net/sandbox/sandbox/Tests.cs
@@ -0,0 +1,18 @@
+﻿
+using NUnit.Framework;
+using System;
+
+namespace sandbox
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Console.Write("Test 1");
+
+            Assert.IsTrue(true);
+        }
+    }
+}
